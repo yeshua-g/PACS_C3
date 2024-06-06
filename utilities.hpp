daaa2f20aaed6850cd5618ba545eadc547b5a0fa
Yeshua Giacalone <yeshua.giacalone@mail.polimi.it>
2024-06-07 01:09:17 +0200

    VTK funct with matrix as vec of vec for now

--- utilities.hpp
+++ utilities.hpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <functional>
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 // Function to create a muParser function from a string expression
 inline std::function<double(double, double)> tofunct(const std::string& expression) {
@@ -24,6 +26,54 @@ inline std::function<double(double, double)> tofunct(const std::string& expressi
         }
     };
 }
+
+void generateVTKFile(const std::string & filename, 
+                     const std::vector<std::vector<double>> & scalarField, 
+                     int nx, int ny, double hx, double hy){
+
+    // Open file
+    std::ofstream vtkFile(filename);
+
+    // Check if file is open
+    if (!vtkFile.is_open()) {
+        std::cerr << "Error: could not open file " << filename << std::endl;
+        return;
+    }
+
+    // Write header
+    vtkFile <<  "# vtk DataFile Version 3.0\n";
+    vtkFile << "Scalar Field Data\n";
+    vtkFile << "ASCII\n";                               
+
+    // Write grid data
+
+    // type of dataset
+    vtkFile << "DATASET STRUCTURED_POINTS\n";      
+    // number of points in each direction                       
+    vtkFile << "DIMENSIONS " << nx+1 << " " << ny+1 << " " << 1 << "\n"; 
+    // origin of the grid 
+    vtkFile << "ORIGIN 0 0 0\n";                                     
+    // spacing between points     
+    vtkFile << "SPACING" << " " << hx << " " << hy << " " << 1 << "\n"; 
+    // number of points  
+    vtkFile << "POINT_DATA " << (nx+1) * (ny+1) << "\n";                  
+                                                                
+    
+    // Write scalar field data
+
+    // name of the scalar field
+    vtkFile << "SCALARS scalars double\n";   
+    // color table            
+    vtkFile << "LOOKUP_TABLE default\n";                
+
+    //write scalar field data
+    for (int j = 0; j < ny+1; j++) {
+        for (int i = 0; i < nx+1; i++) {
+            vtkFile <<  scalarField[i][j] << "\n";
+        }
+    }
+
+};
 
 
 
